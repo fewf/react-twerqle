c2b16031beec0535d70c9d79c755be4e7d451542
greggreggreg7 <g.s.altman@gmail.com>
2014-11-19 14:30:18 -0500

    update

--- assets/main.js
+++ assets/main.js
@@ -14954,7 +14954,7 @@
 	        var top = -1 * (svgLength/2 - windowDims.y/2);
 
 	        var $board = this.getDOMNode();
-	        $boardObjects = this.refs.boardObjects.getDOMNode();
+	        var $boardObjects = this.refs.boardObjects.getDOMNode();
 
 	        $board.style.left = left;
 	        $board.style.top = top;
