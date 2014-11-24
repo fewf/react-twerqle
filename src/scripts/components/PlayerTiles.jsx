2d78de06b85bd1aacf43721a823bca1e1a59465c
greggreggreg7 <g.s.altman@gmail.com>
2014-11-24 13:02:46 -0500

    added sort button functionality, separated buttons

--- src/scripts/components/PlayerTiles.jsx
+++ src/scripts/components/PlayerTiles.jsx
@@ -10,15 +10,6 @@ var PlayerTile = require('./PlayerTile');
 require('../../styles/PlayerTiles.css');
 
 var PlayerTiles = React.createClass({
-    // getInitialState: function() {
-        // var placeholder = document.createElement("li");
-        // placeholder.className = "player-tile"
-        // placeholder.id = "placeholder";
-        // placeholder.addEventListener("drop", this.tileDrop)
-        // return {
-        //     placeholder: placeholder,
-        // }
-    // },
     render: function() {
         var tiles = this.props.tiles.map(function (tile, i) {
             return (
@@ -34,23 +25,6 @@ var PlayerTiles = React.createClass({
         return (<ul id="rack">
                     {tiles}
                 </ul>);
-    },
-    componentWillReceiveProps: function(nextProps) {
-
-        // var orderedTiles = this.state.orderedTiles;
-        // var propTiles = nextProps.tiles.slice();
-        // var reconciled = [];
-
-        // for (var i = 0; i < orderedTiles.length; i++) {
-        //     var found = propTiles.indexOf(orderedTiles[i]);
-        //     if (found != -1) {
-        //         reconciled.push(orderedTiles[i]);
-        //         propTiles.splice(found, 1);
-        //     }
-        // };
-
-        // var reconciled = reconciled.concat(propTiles);
-        // this.setState({orderedTiles: reconciled});
     }
 });
 
